77890a5b1dfbe73bffef2b80cc52b2dedfa48246
Gizzat Tazabekov <gizzat.tazabekov@c3iot.com>
2018-02-17 01:17:00 -0800

    lit-html template support for b4-table

--- app/app.js
+++ app/app.js
@@ -1,11 +1,1 @@
-import { html, render } from '../node_modules/lit-html/lit-html.js';
-
-const greeting = (prefix, name) => {
-    return html`
-    <h3>test ${prefix} ${name}</h3>
-  `;
-};
-
-const el = document.querySelector('#container');
-
-render(greeting('1', '2'), el);
+import B4Table from "./components/b4-table/b4-table.js";
--- app/components/b4-table/b4-table.js
+++ app/components/b4-table/b4-table.js
@@ -1,1 +1,69 @@
 import { html, render } from '../../../node_modules/lit-html/lit-html.js';
+
+export default class B4Table extends HTMLElement {
+    constructor(){
+        super();
+        this.attachShadow({mode: 'open'});
+        this.counterValue = 3;
+        this.users = [
+            {id: 1, email: "gizzat.tazabekov@c3iot.com", fname: "Gizzat", lname: "Tazabekov"},
+            {id: 2, email: "juan.castaneda@c3iot.com", fname: "Juan", lname: "Castañeda"}
+        ];
+    }
+
+    connectedCallback() {
+        // fetch(`app/components/${B4Table.is}/${B4Table.is}.html`)
+        // .then(fetchResponse => fetchResponse.text())
+        // .then(templateText => {
+        //     const templateEl = new DOMParser().parseFromString(templateText, 'text/html')
+        // });
+        this.render();
+
+        setInterval(_=>{
+            this.counterValue++;
+            this.render();
+        }, 1000);
+    }
+
+
+    template(){
+        return html`
+            <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
+            <link rel="stylesheet" href="app/components/${B4Table.is}/${B4Table.is}.css">
+            <table class="table table-hover">
+                <thead class="thead-dark">
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">First</th>
+                    <th scope="col">Last</th>
+                </tr>
+                </thead>
+                <tbody>
+                ${this.users.map((u, i) => html`
+                    <tr>
+                        <th scope="row">${u.id}</th>
+                        <td>${u.email}</td>
+                        <td>${u.fname}</td>
+                        <td>${u.lname}</td>
+                    </tr>
+                `)}
+                <tr>
+                    <th scope="row">hello(${this.counterValue})</th>
+                    <td>gizzat.tazabekov@c3iot.com</td>
+                    <td>Gizzat</td>
+                    <td>Tazabekov</td>
+                </tr>
+                </tbody>
+            </table>
+        `;
+    }
+
+    render(){
+        render(this.template(), this.shadowRoot);
+    }
+
+    static get is(){ return "b4-table"; }
+}
+
+customElements.define(B4Table.is, B4Table);
