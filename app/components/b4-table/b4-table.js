0d50afc189cb8e56e634bfe6449464b50aeaa818
Gizzat Tazabekov <gizzat.tazabekov@c3iot.com>
2018-02-18 00:34:34 -0800

    separate html file

--- app/components/b4-table/b4-table.js
+++ app/components/b4-table/b4-table.js
@@ -12,55 +12,23 @@ export default class B4Table extends HTMLElement {
     }
 
     connectedCallback() {
-        // fetch(`app/components/${B4Table.is}/${B4Table.is}.html`)
-        // .then(fetchResponse => fetchResponse.text())
-        // .then(templateText => {
-        //     const templateEl = new DOMParser().parseFromString(templateText, 'text/html')
-        // });
-        this.render();
-
-        setInterval(_=>{
-            this.counterValue++;
+        fetch(`app/components/${B4Table.is}/${B4Table.is}.html`)
+        .then(fetchResponse => fetchResponse.text())
+        .then(html => {
+            this.template = html;
             this.render();
-        }, 1000);
+
+            setInterval(_=>{
+                this.counterValue++;
+                this.render();
+            }, 1000);
+        });
     }
 
 
-    template(){
-        return html`
-            <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
-            <link rel="stylesheet" href="app/components/${B4Table.is}/${B4Table.is}.css">
-            <table class="table table-hover">
-                <thead class="thead-dark">
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Email</th>
-                    <th scope="col">First</th>
-                    <th scope="col">Last</th>
-                </tr>
-                </thead>
-                <tbody>
-                ${this.users.map((u, i) => html`
-                    <tr>
-                        <th scope="row">${u.id}</th>
-                        <td>${u.email}</td>
-                        <td>${u.fname}</td>
-                        <td>${u.lname}</td>
-                    </tr>
-                `)}
-                <tr>
-                    <th scope="row">hello(${this.counterValue})</th>
-                    <td>gizzat.tazabekov@c3iot.com</td>
-                    <td>Gizzat</td>
-                    <td>Tazabekov</td>
-                </tr>
-                </tbody>
-            </table>
-        `;
-    }
 
     render(){
-        render(this.template(), this.shadowRoot);
+        render(eval("html`"+this.template+"`"), this.shadowRoot);
     }
 
     static get is(){ return "b4-table"; }
